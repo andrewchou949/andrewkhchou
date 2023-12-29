a14a0004c7266c2ec36faa93053b8cf3a29c146d
Andrew <andrewchou949@gmail.com>
2023-12-29 01:03:07 -0800

    Updating fade in animation when page loaded

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -43,6 +43,7 @@ function useTypingEffect(words: string[], speed = 175, delay = 3000) {
   }
   
   const Home = () => {
+    /* All these are the fading out animation while scrolling! */
     const [h1Style, setH1Style] = useState({});
     const [h2Style, setH2Style] = useState({});
     const [h3Style, setH3Style] = useState({});
@@ -80,19 +81,36 @@ function useTypingEffect(words: string[], speed = 175, delay = 3000) {
     const [currentSkill, setCurrentSkill] = useState('');
     const skills = ["Python", "C++", "Java", "SQLite", "Dart", "Technical Writing", "Firebase", "FlutterFlow", "Canva", " Lucid Chart"];
     const typingSkill = useTypingEffect(skills, 175, 3000);
-  
+    /* For the last line (typing animation) */
     useEffect(() => {
       setCurrentSkill(typingSkill);
     }, [typingSkill]);
-  
+
+    /* Fading in animation when page loads */
+    // States for controlling the initial fade-in animation
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [isSecondaryContentVisible, setIsSecondaryContentVisible] = useState(false);
+
+    useEffect(() => {
+      setIsLoaded(true); // Triggers fade-in effect for h1 and h2 when the component mounts
+      setTimeout(() => {
+        setIsSecondaryContentVisible(true); // Triggers fade-in effect for h3, p, and SocialIcons after a delay
+      }, 1000); // Adjust this delay to control when the secondary content starts fading in (1000ms = 1s delay)
+    }, []);
+
+
     return (
-      <div id="home" className="section"> {/*  style={{ backgroundImage: `url(${backgroundImage})` }} */}
-        {/* Content for home! */}
-        <h1 style={h1Style}>Hello, I'm</h1>
-        <h2 style={h2Style}>Kimhour (Andrew) Chou</h2>
-        <h3 style={h3Style}>Recent CSSE Undergrad Student</h3>
-        <p style={pStyle}>{currentSkill}</p>
-        <SocialIcons />
+      <div id="home" className="section">
+      {/* Content for home! */}
+      {/* Fade in when page loaded */}
+        <h1 className={`${isLoaded ? 'fadeIn' : ''}`} style={h1Style}>Hello, I'm</h1>
+        <h2 className={`${isLoaded ? 'fadeIn' : ''}`} style={h2Style}>Kimhour (Andrew) Chou</h2>
+        {/* Secondary content to fade in after h1 and h2 */}
+        <div className={`${isSecondaryContentVisible ? 'fadeInLater' : 'hide'}`}>
+          <h3 style={h3Style}>Recent CSSE Undergrad Student</h3>
+          <p style={pStyle}>{currentSkill}</p>
+          <SocialIcons />
+        </div>
       </div>
     );
   };
