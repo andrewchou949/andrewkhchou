9016946993dfa3995a12200a0163e3b8cf1246e6
Andrew <andrewchou949@gmail.com>
2024-05-31 22:41:52 -0700

    Added one more project and uploaded updated resume

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -6,21 +6,22 @@ import projectImage2 from '../../backgroundImage/Language Application.png'
 import projectImage3 from '../../backgroundImage/Database.png'
 import projectImage4 from '../../backgroundImage/VoiceRecognition.png'
 import projectImage5 from '../../backgroundImage/discordbot.png'
+import projectImage6 from '../../backgroundImage/finance.png'
 
 const projectsData =[
     {
-    title: 'Personal Self Introduction Webpage', 
-    description: 'Utilizing React, TypeScript, Firebase, and AWS Route 53, this personal website project is a dynamic showcase of web development and UI/UX design expertise. Experience a seamless, feature-rich user journey that highlights the integration of these cutting-edge tools.',
-    demoLink: 'https://github.com/andrewchou949/andrewkhchou',
-    imageUrl: projectImage1,
-    skills: ['TypeScript', 'Firebase', 'React', 'AWS Route 53', 'DALL-E 3', 'React Spring Parallax', 'Responsive Design']
+    title: 'Personal Finance Management Application with Fraud Detection',
+    description: 'Developing an application to help users track expenses and detect fraudulent transactions using machine learning. Implementing user authentication, expense tracking, data visualization, and fraud detection alerts. Utilizing technologies such as Django for backend, React for frontend, and machine learning models for fraud detection. Hosting on AWS for scalable and secure deployment.',
+    demoLink: 'https://github.com/andrewchou949/Personal-Finance-Management-Application',
+    imageUrl: projectImage6,
+    skills: ['Python', 'Django', 'React', 'AWS', 'Machine Learning', 'Data Visualization', 'RESTful API']
     },
     {
-    title: 'Polyglot Go', 
-    description: 'Polyglot Go, a text-based language learning app, acts as a bridge to enhance language acquisition. This versatile tool offers an engaging experience across various platforms, including iPad, iPhone, and web browsers. Packed with features like Flashcards, Translation, and Word-matching, learners have a robust toolkit at their disposal to enrich their language learning journey.',
-    demoLink: 'https://github.com/andrewchou949/Polyglot-Go',
-    imageUrl: projectImage2,
-    skills: ['FlutterFlow', 'Firebase', 'Dart', 'Lucid Chart', 'Canva', 'Technical Writing']
+    title: 'Voice Recognition/Summary Web App',
+    description: 'Leverating the OpenAI Whisper model for precise voice recognition, combined with ChatGPT-3.5 Turbo for efficient summary creation, setting a new standard in user-focused applications. Hosted with Google Cloud CLI for dependable backend operation, scalable API service delivery. The frontend, crafted with Create React App (CRA) and elegantly styled with Tailwind CSS, offers an intuitive and visually appealing user interface.',
+    demoLink: 'https://vrfrontend.web.app/',
+    imageUrl: projectImage4,
+    skills: ['React', 'Tailwind CSS', 'Flask', 'Google Cloud', 'Openai-Whisper', 'ChatGPT3.5-Turbo', 'RESTful API']
     },
     {
     title: 'Discord Bot - Andrew\'s Bot',
@@ -30,11 +31,18 @@ const projectsData =[
     skills: ['JavaScript', 'discord.js', 'RESTful API', 'Project Management']
     },
     {
-    title: 'Voice Recognition/Summary Web App',
-    description: 'Leverating the OpenAI Whisper model for precise voice recognition, combined with ChatGPT-3.5 Turbo for efficient summary creation, setting a new standard in user-focused applications. Hosted with Google Cloud CLI for dependable backend operation, scalable API service delivery. The frontend, crafted with Create React App (CRA) and elegantly styled with Tailwind CSS, offers an intuitive and visually appealing user interface.',
-    demoLink: 'https://vrfrontend.web.app/',
-    imageUrl: projectImage4,
-    skills: ['React', 'Tailwind CSS', 'Flask', 'Google Cloud', 'Openai-Whisper', 'ChatGPT3.5-Turbo', 'RESTful API']
+    title: 'Polyglot Go', 
+    description: 'Polyglot Go, a text-based language learning app, acts as a bridge to enhance language acquisition. This versatile tool offers an engaging experience across various platforms, including iPad, iPhone, and web browsers. Packed with features like Flashcards, Translation, and Word-matching, learners have a robust toolkit at their disposal to enrich their language learning journey.',
+    demoLink: 'https://github.com/andrewchou949/Polyglot-Go',
+    imageUrl: projectImage2,
+    skills: ['FlutterFlow', 'Firebase', 'Dart', 'Lucid Chart', 'Canva', 'Technical Writing']
+    },
+    {
+    title: 'Personal Self Introduction Webpage', 
+    description: 'Utilizing React, TypeScript, Firebase, and AWS Route 53, this personal website project is a dynamic showcase of web development and UI/UX design expertise. Experience a seamless, feature-rich user journey that highlights the integration of these cutting-edge tools.',
+    demoLink: 'https://github.com/andrewchou949/andrewkhchou',
+    imageUrl: projectImage1,
+    skills: ['TypeScript', 'Firebase', 'React', 'AWS Route 53', 'DALL-E 3', 'React Spring Parallax', 'Responsive Design']
     },
     {
     title: 'Bank System Database',
